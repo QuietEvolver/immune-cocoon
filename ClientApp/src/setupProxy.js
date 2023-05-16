91e27f7edd68d2e28bf8fd787f2028abad02e619
Vera Weikel <vera.weikel@gmail.com>
2023-05-16 11:11:58 -0700

    Add /api/businesses to setupProxy.js. Live version TBD.

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -8,6 +8,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 const context =  [
   "/weatherforecast",
+  // "/api/businesses",
   "/_configuration",
   "/.well-known",
   "/Identity",
