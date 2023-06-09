bfeb976becefa7a0e437957eee3fbe063ca572ef
Vera Weikel <vera.weikel@gmail.com>
2023-06-09 15:52:35 -0700

    Add notes for upcoming CRUD Testing.

--- xunit/UnitTest1.cs
+++ xunit/UnitTest1.cs
@@ -5,6 +5,8 @@ namespace xunittesting;
 //     [Fact]
 //     public void Test1()
 //     {
-
+// Will potentially attempt to loop in Selenium
+// Otherwise, will use xunittesting pkgs for CRUDf fxnlty.
+// test by xpaths and assert.equal(expected, assert);
 //     }
 // }
